eeedd277c86af7d2328c5df580968ea125882fe7
front42 <altaifront@gmail.com>
2024-03-05 20:07:19 +0300

    feat: add launch on correct port with instructing main route

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `<h2>Continue your request with /user, /track, /artist, etc...</h2>
+    <a href="https://github.com/AlreadyBored/nodejs-assignments/blob/main/assignments/rest-service/assignment.md">
+      <h1>Full list of routes</h1>
+    </a>`;
   }
 }
